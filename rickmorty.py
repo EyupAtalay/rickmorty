8a5dadbd6a6ae63dcb4c201954c89e145e6a171d
Eyüp Atalay <104161770+EyupAtalay@users.noreply.github.com>
2023-08-04 23:53:42 +0300

    first commit

---
+++ rickmorty.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://rickandmortyapi.com/api/character"
+params = {'name': 'Morty'}  # 'name' alanı 'Morty' olan karakterleri filtrelemek için
+
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    for character in data['results']:
+    
+        print(character['name'])
+else:
+    print("İstek başarısız oldu. Hata kodu:", response.status_code)
